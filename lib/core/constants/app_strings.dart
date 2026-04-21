56c5225e0fa88baafabaa89f7bb44e005b60425c
ginaa07 <1123150124@global.ac.id>
2026-04-22 00:24:41 +0700

    feat: Core Constants & Service
    create app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,14 @@
+class AppStrings {
+  // App
+  static const String appName = 'Toko Sepatu Heels Wanita';
+
+  // Auth
+  static const String register = 'Daftar';
+  static const String login = 'Masuk';
+  static const String logout = 'Keluar';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String confirmPassword = 'Konfirmasi Password';
+  static const String fullName = 'Nama Lengkap';
+  static const String forgotPassword = 'Lupa Password?';
+  static const String resetPassword = 'Reset Password';
