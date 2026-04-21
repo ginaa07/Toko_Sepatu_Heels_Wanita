fc05ea2778a83125d0d718a6ecc0f1f03bda0874
ginaa07 <1123150124@global.ac.id>
2026-04-22 01:17:08 +0700

    feat: Reusable Widgets
    core\features\...\widgets create custom_text_field.dart

---
+++ lib/features/auth/data/domain/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget { 
+  final String label; 
+  final String hint; 
+  final TextEditingController controller; 
+  final bool obscureText; 
+  final String? Function(String?)? validator; 
+  final TextInputType keyboardType; 
+  final Widget? suffixIcon; 
+  final Widget? prefixIcon; 
+  final bool enabled; 
+ 
+  const CustomTextField({ 
+    super.key, 
+    required this.label, 
+    required this.hint, 
+    required this.controller, 
+    this.obscureText = false, 
+    this.validator, 
+    this.keyboardType = TextInputType.text, 
+    this.suffixIcon, 
+    this.prefixIcon, 
+    this.enabled = true, 
+  }); 
