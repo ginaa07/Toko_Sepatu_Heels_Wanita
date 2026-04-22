fdcfa1d21650da248cba4397d0c880212ca0f694
ginaa07 <1123150124@global.ac.id>
2026-04-22 14:57:23 +0700

    fix: update in main.dart
    main.dart

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toko_sepatu_heels_wanita/core/services/secure_storage.dart';
+import 'package:toko_sepatu_heels_wanita/core/theme/app_theme.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/login_page.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/register_page.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/verify_email_page.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/providers/auth_provider.dart';
-import 'package:toko_sepatu_heels_wanita/main.dart';
+
 
 
 
