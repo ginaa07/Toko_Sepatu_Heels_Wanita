0e27fa9b48f46b934f9ef1500686bd08f19d9962
ginaa07 <1123150124@global.ac.id>
2026-04-22 14:46:58 +0700

    feat: Core Constants & Service
    core\routes add class MyApp in app_router.dart

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/login_page.dart';
+import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/register_page.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/pages/verify_email_page.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/providers/auth_provider.dart';
+import 'package:toko_sepatu_heels_wanita/main.dart';
 
 
 
@@ -59,6 +61,27 @@ class AuthGuard extends StatelessWidget {
     }; 
   } 
 } 
+
+class MyApp extends StatelessWidget { 
+  const MyApp({super.key}); 
+ 
+  @override 
+  Widget build(BuildContext context) { 
+    return MultiProvider( 
+      providers: [ 
+         ChangeNotifierProvider(create: (_) => AuthProvider()), 
+        ChangeNotifierProvider(create: (_) => ProductProvider()), 
+      ], 
+      child: MaterialApp( 
+        title:                  'My App', 
+        debugShowCheckedModeBanner: false, 
+        theme:                  AppTheme.light, 
+        initialRoute:           AppRouter.splash, 
+        routes:                 AppRouter.routes, 
+      ), 
+    ); 
+  } 
+}
 }
 
 
