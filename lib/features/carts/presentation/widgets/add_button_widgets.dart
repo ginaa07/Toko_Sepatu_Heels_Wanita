e9a3770ae68a2c0d8ea64843bb9dedda47cdb2e8
ginaa07 <1123150124@global.ac.id>
2026-04-23 14:08:12 +0700

    feat: create folder carts\...\presentation\widgets
    create file add_button_widgets.dart

---
+++ lib/features/carts/presentation/widgets/add_button_widgets.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toko_sepatu_heels_wanita/features/carts/domain/repositories/product.dart';
+import 'package:toko_sepatu_heels_wanita/features/carts/presentation/providers/cart_provider.dart';
+
+class AddButtonWidget extends StatelessWidget {
+  final Product product;
+  const AddButtonWidget({required this.product, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isInCart = context.select<CartProvider, bool>(
+      (provider) => provider.isInCart(product.id),
+    );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () => context.read<CartProvider>().addItem(product),
+      child: isInCart
+          ? const Icon(Icons.check, color: Colors.green)
+          : const Text('TAMBAH'),
+    );
+  }
+}
