8a38be1edb890d1f4c930fa981ba23465d1c0f64
ginaa07 <1123150124@global.ac.id>
2026-04-23 15:40:15 +0700

    fix: update in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:toko_sepatu_heels_wanita/core/routes/app_router.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/providers/auth_provider.dart';
+import 'package:toko_sepatu_heels_wanita/features/carts/domain/repositories/cart_repository_impl.dart';
 import 'package:toko_sepatu_heels_wanita/features/carts/presentation/providers/cart_provider.dart';
 import 'package:toko_sepatu_heels_wanita/features/dashboard/presentation/providers/product_provider.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:toko_sepatu_heels_wanita/features/carts/data/domain/repositories/cart_repository_impl.dart';
 
 
 
@@ -23,7 +23,7 @@ void main() async {
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ProductProvider()),
-        ChangeNotifierProvider(create: (_) => CartProvider(repository: CartRepositoryImpl()),
+        ChangeNotifierProvider(create: (_) => CartProvider(repository: CartRepositoryImpl())),
       ],
       child: const MyApp(),
     ),
