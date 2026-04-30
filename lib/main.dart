84c4fb9908aa540952fac9fdfe8831fc6f0dbebf
ginaa07 <1123150124@global.ac.id>
2026-05-01 00:08:27 +0700

    fix: update code for final themeprovider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toko_sepatu_heels_wanita/core/providers/theme_provider.dart';
 import 'package:toko_sepatu_heels_wanita/core/routes/app_router.dart';
+import 'package:toko_sepatu_heels_wanita/core/theme/app_theme.dart';
 import 'package:toko_sepatu_heels_wanita/features/auth/data/domain/presentation/providers/auth_provider.dart';
 import 'package:toko_sepatu_heels_wanita/features/carts/domain/repositories/cart_repository_impl.dart';
 import 'package:toko_sepatu_heels_wanita/features/carts/presentation/providers/cart_provider.dart';
 import 'package:toko_sepatu_heels_wanita/features/dashboard/presentation/providers/product_provider.dart';
-import 'firebase_options.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MultiProvider(
@@ -21,6 +26,7 @@ void main() async {
         ChangeNotifierProvider(
           create: (_) => CartProvider(repository: CartRepositoryImpl()),
         ),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()), 
       ],
       child: const MyApp(),
     ),
@@ -32,14 +38,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     final themeProvider = context.watch<ThemeProvider>(); 
+    final themeProvider = context.watch<ThemeProvider>(); 
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Toko Sepatu Heels Wanita',
 
-      theme: AppTheme.light, 
-      darkTheme: AppTheme.dark, 
-      themeMode: ThemeMode.system, 
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      themeMode: themeProvider.themeMode, 
 
       initialRoute: AppRouter.splash,
       routes: AppRouter.routes,
