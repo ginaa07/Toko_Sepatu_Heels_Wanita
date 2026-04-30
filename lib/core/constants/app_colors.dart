d4291cd601771f69ddc057d81eb5d958026be8df
ginaa07 <1123150124@global.ac.id>
2026-04-30 23:24:42 +0700

    fix: update code for add dark mode and light mode in app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color primary = Color(0xFF1565C0);
   static const Color primaryLight = Color(0xFF1E88E5);
   static const Color primaryDark = Color(0xFF0D47A1);
-  static const Color accent = Color(0xFF64B5F6);
+  static const Color accent = Color(0xFF42A5F5);
 
   //Light mode
   static const Color surface = Colors.white;
